984443d9afbf7f2548f63842ce38c3db5d60bc55
wy <123456@163.com>
2020-01-05 17:23:09 +0800

    关联了Redis

--- src/main/java/com/woniu/app/App.java
+++ src/main/java/com/woniu/app/App.java
@@ -3,9 +3,11 @@ package com.woniu.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = "com.woniu" )
 @MapperScan("com.woniu.dao")
+@EnableAspectJAutoProxy
 public class App {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/woniu/config/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.woniu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfig {
+	@Bean
+	public JedisPool redisPoolFactory() throws Exception{
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig,"192.168.9.233",6379);
+		return jedisPool;
+	}
+}
